b427681a47f88e59c112ec2ef4ff98c6f384d0ae
Samuel James <samuel4abiodun@gmail.com>
2021-03-21 11:30:00 +0100

    Initial commit

---
+++ output/model/automl-fraudcase-21-10-06-37/sagemaker-automl-candidates/pr-1-bfb21b0c8b8347ebb3402853908b18c1df4ad627cb764fd1918a382b94/generated_module/candidate_data_processors/dpp2.py
@@ -0,0 +1,89 @@
+from sagemaker_sklearn_extension.decomposition import RobustPCA
+from sagemaker_sklearn_extension.externals import Header
+from sagemaker_sklearn_extension.impute import RobustImputer
+from sagemaker_sklearn_extension.impute import RobustMissingIndicator
+from sagemaker_sklearn_extension.preprocessing import QuantileExtremeValuesTransformer
+from sagemaker_sklearn_extension.preprocessing import RobustLabelEncoder
+from sagemaker_sklearn_extension.preprocessing import RobustStandardScaler
+from sagemaker_sklearn_extension.preprocessing import ThresholdOneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+
+# Given a list of column names and target column name, Header can return the index
+# for given column name
+HEADER = Header(
+    column_names=[
+        'Unnamed: 0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9',
+        'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19',
+        'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Class',
+        'amt'
+    ],
+    target_column_name='Class'
+)
+
+
+def build_feature_transform():
+    """ Returns the model definition representing feature processing."""
+
+    # These features can be parsed as numeric.
+
+    numeric = HEADER.as_feature_indices(
+        [
+            'Unnamed: 0', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9',
+            'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18',
+            'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27',
+            'V28', 'amt'
+        ]
+    )
+
+    # These features contain a relatively small number of unique items.
+
+    categorical = HEADER.as_feature_indices(['amt'])
+
+    numeric_processors = Pipeline(
+        steps=[
+            (
+                'featureunion',
+                FeatureUnion(
+                    [
+                        ('robust_imputer', RobustImputer()),
+                        ('robust_missing_indicator', RobustMissingIndicator())
+                    ]
+                )
+            ),
+            (
+                'quantileextremevaluestransformer',
+                QuantileExtremeValuesTransformer()
+            )
+        ]
+    )
+
+    categorical_processors = Pipeline(
+        steps=[
+            ('thresholdonehotencoder', ThresholdOneHotEncoder(threshold=31))
+        ]
+    )
+
+    column_transformer = ColumnTransformer(
+        transformers=[
+            ('numeric_processing', numeric_processors, numeric
+            ), ('categorical_processing', categorical_processors, categorical)
+        ]
+    )
+
+    return Pipeline(
+        steps=[
+            ('column_transformer',
+             column_transformer), ('robustpca', RobustPCA(n_components=147)),
+            ('robuststandardscaler', RobustStandardScaler())
+        ]
+    )
+
+
+def build_label_transform():
+    """Returns the model definition representing feature processing."""
+
+    return RobustLabelEncoder(
+        labels=['0'], fill_label_value='1', include_unseen_class=True
+    )
